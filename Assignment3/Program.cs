ce27fb03948ebbb858b5fd9c46707a611eb7d955
adamjhr <adam.jh.rose6@gmail.com>
2022-09-27 12:30:32 +0200

    Sqllite

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -3,18 +3,27 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Data.SqlClient;
+using Microsoft.Data.Sqlite;
 
 
 var configuration = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
 // var connectionString = configuration.GetConnectionString("Kanban"); // Eller skal det være "ConnectionString"
-var connectionString = "User ID=postgres;Password=adam123;Data Source=localhost,5432;Database=imdb;Pooling=true;Min Pool Size=0; Max Pool Size=100;TrustServerCertificate=true;Timeout=1000000";
+// var connectionString = "User ID=postgres;Password=adam123;Data Source=localhost,5432;Database=imdb;Pooling=true;Min Pool Size=0; Max Pool Size=100;TrustServerCertificate=true;Timeout=1000000";
 
+//using var connection = new SqlConnection(connectionString);
+using var connection = new SqliteConnection("Data source = test.db");
 
-
-using var connection = new SqlConnection(connectionString);
-using var getTable = new SqlCommand(@"SELECT * FROM testtable", connection);
+var createTable = connection.CreateCommand();
+var getTable = connection.CreateCommand();
+var addToTable = connection.CreateCommand();
+createTable.CommandText = @"CREATE TABLE test (name varchar(10))";
+addToTable.CommandText = @"INSERT INTO test VALUES ('word'), ('word2')";
+getTable.CommandText = @"SELECT * FROM test";
 
 connection.Open();
+
+createTable.ExecuteNonQuery();
+addToTable.ExecuteNonQuery();
 
 using var reader = getTable.ExecuteReader();
 
