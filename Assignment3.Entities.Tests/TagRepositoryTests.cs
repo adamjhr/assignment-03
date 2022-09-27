eae9d749a01d932fdac529a7d7b50e6dc4e91eb8
adamjhr <adam.jh.rose6@gmail.com>
2022-09-27 14:02:11 +0200

    TagRepository tests made

--- Assignment3.Entities.Tests/TagRepositoryTests.cs
+++ Assignment3.Entities.Tests/TagRepositoryTests.cs
@@ -17,4 +17,62 @@ public class TagRepositoryTests
 
     }
 
+    [Fact]
+    public void ReadNonExistantReturnsNull() {
+        
+        var factory = new KanbanContextFactory();
+        var context = factory.CreateDbTestContext(null);
+        TagRepository tagRepo = new TagRepository(context);
+
+        var response = tagRepo.Read(1);
+
+        response.Should().BeNull();
+
+    }
+
+    [Fact]
+    public void TagAssignedToTaskReturnsConflict() {
+
+        var factory = new KanbanContextFactory();
+        var context = factory.CreateDbTestContext(null);
+        TagRepository tagRepo = new TagRepository(context);
+        TaskRepository taskRepo = new TaskRepository(context);
+        var tag = tagRepo.Create(new Core.TagCreateDTO("testTag"));
+        taskRepo.Create(new Core.TaskCreateDTO("testTask", null, null, new List<string> { "testTag" }));
+    
+        var response = tagRepo.Delete(tag.TagId, false);
+
+        response.Should().Be(Core.Response.Conflict);
+    
+    }
+
+    [Fact]
+    public void TagAssignedToTaskReturnsDeletedWhenForced() {
+
+        var factory = new KanbanContextFactory();
+        var context = factory.CreateDbTestContext(null);
+        TagRepository tagRepo = new TagRepository(context);
+        TaskRepository taskRepo = new TaskRepository(context);
+        var tag = tagRepo.Create(new Core.TagCreateDTO("testTag"));
+        taskRepo.Create(new Core.TaskCreateDTO("testTask", null, null, new List<string> { "testTag" }));
+    
+        var response = tagRepo.Delete(tag.TagId, true);
+
+        response.Should().Be(Core.Response.Deleted);
+    
+    }
+
+    [Fact]
+    public void CreateSameTagTwiceReturnsConflict() {
+        var factory = new KanbanContextFactory();
+        var context = factory.CreateDbTestContext(null);
+        TagRepository tagRepo = new TagRepository(context);
+        tagRepo.Create(new Core.TagCreateDTO("testTag"));
+        
+        var tag = tagRepo.Create(new Core.TagCreateDTO("testTag"));
+        var response = tag.Response;
+
+        response.Should().Be(Core.Response.Conflict);
+    }
+
 }
