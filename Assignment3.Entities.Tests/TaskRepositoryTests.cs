358fe1b8b34762118361a7e534c57c99cc17f833
adamjhr <adam.jh.rose6@gmail.com>
2022-09-28 15:26:42 +0200

    More tests

--- Assignment3.Entities.Tests/TaskRepositoryTests.cs
+++ Assignment3.Entities.Tests/TaskRepositoryTests.cs
@@ -3,4 +3,30 @@ namespace Assignment3.Entities.Tests;
 public class TaskRepositoryTests
 {
 
+    [Fact]
+    public void DeleteWithStateNewReturnsDeleted() {
+
+        var factory = new KanbanContextFactory();
+        var context = factory.CreateDbTestContext(null);
+        var taskRepo = new TaskRepository(context);
+        var task = taskRepo.Create(new Core.TaskCreateDTO("testTask", null, null, new List<string> {}));
+        
+        var response = taskRepo.Delete(task.TaskId);
+
+        response.Should().Be(Core.Response.Deleted);
+    }
+
+    [Fact]
+    public void StateIsNewUponCreation() {
+
+        var factory = new KanbanContextFactory();
+        var context = factory.CreateDbTestContext(null);
+        var taskRepo = new TaskRepository(context);
+        var task = taskRepo.Create(new Core.TaskCreateDTO("testTask", null, null, new List<string> {}));
+        
+        var response = taskRepo.Read(task.TaskId).State;
+
+        response.Should().Be(Core.State.New);
+    }
+
 }
