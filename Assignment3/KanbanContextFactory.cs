700f2395d17f5fc227418a2fc9e4f7ebddc15f9b
adamjhr <adam.jh.rose6@gmail.com>
2022-09-29 12:32:26 +0200

    Changed to proper in memory database

--- Assignment3/KanbanContextFactory.cs
+++ Assignment3/KanbanContextFactory.cs
@@ -8,16 +8,24 @@ namespace Assignment3;
 
 public class KanbanContextFactory : IDesignTimeDbContextFactory<KanbanContext> {
 
+    // public KanbanContext CreateDbContext(string[] args) {
+
+    //     var configuration = new ConfigurationBuilder()
+    //         .AddUserSecrets(typeof(KanbanContext).Assembly, true)
+    //         .Build();
+    //     var connectionString = configuration.GetConnectionString("ConnectionString");
+
+    //     var optionsBuilder = new DbContextOptionsBuilder<KanbanContext>();
+    //     optionsBuilder.UseLazyLoadingProxies().UseNpgsql(connectionString);
+
+
+    //     return new KanbanContext(optionsBuilder.Options);
+    // }
+
     public KanbanContext CreateDbContext(string[] args) {
 
-        var configuration = new ConfigurationBuilder()
-            .AddUserSecrets(typeof(KanbanContext).Assembly, true)
-            .Build();
-        var connectionString = configuration.GetConnectionString("ConnectionString");
-
         var optionsBuilder = new DbContextOptionsBuilder<KanbanContext>();
-        optionsBuilder.UseLazyLoadingProxies().UseNpgsql(connectionString);
-
+        optionsBuilder.UseSqlite("Data Source=:memory:;");
 
         return new KanbanContext(optionsBuilder.Options);
     }
@@ -25,7 +33,7 @@ public class KanbanContextFactory : IDesignTimeDbContextFactory<KanbanContext> {
     public KanbanContext CreateDbTestContext(string[] args) {
 
         var optionsBuilder = new DbContextOptionsBuilder<KanbanContext>();
-        optionsBuilder.UseSqlite();
+        optionsBuilder.UseInMemoryDatabase("db");
 
         return new KanbanContext(optionsBuilder.Options);
     }
